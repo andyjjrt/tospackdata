d47f618d5f0f41317f7c11fc0d696a10da18594f
andyjjrt <andyjjrt@gmail.com>
2021-06-05 21:22:59 +0800

    init

---
+++ fetch.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import json5 as json5
+import json
+
+if not os.path.isdir("data"):
+	os.mkdir("data")
+
+uid = input("uid: ")
+auth = input ("auth: ")
+with open('data/data.json', 'w') as f:
+    f.write('{"uid":"' + str(uid) + '", "auth":"' + str(auth) + '"}')
+
+soup = BeautifulSoup(requests.get("https://checkup.tosgame.com/").text, 'html.parser')
+script = soup.find_all('script')
+for src in script:
+    if "main" in str(src.get('src')):
+        target_url = str(src.get('src'))
+
+res = requests.get("https://checkup.tosgame.com" + target_url).text
+monster_data = json.dumps(json5.loads(res.split("var n=new Map(")[1].split(".map((function(a){return[a.id,a]})))}")[0].replace("!", "")))
+monster_active = json.dumps(json5.loads(res.split("var n=new Map(")[2].split(".map((function(a){return[a[0],{name:JSON.parse(a[1].name),description:JSON.parse(a[1].description)}]})))}")[0]))
+monster_leader = json.dumps(json5.loads(res.split("var n=new Map(")[3].split(".map((function(a){return[a[0],{name:JSON.parse(a[1].name),description:JSON.parse(a[1].description)}]})))}")[0]))
+monster_team = json.dumps(json5.loads(res.split("var n=new Map(")[4].split(".map((function(a){return[a[0],JSON.parse(a[1])]})))}")[0]))
+
+with open('data/MONSTER_DATA.json', 'w') as f:
+    f.write(monster_data)
+with open('data/MONSTER_LEADER.json', 'w') as f:
+    f.write(monster_leader)
+with open('data/MONSTER_TEAM.json', 'w') as f:
+    f.write(monster_team)
+with open('data/MONSTER_ACTIVE.json', 'w') as f:
+    f.write(monster_active)
