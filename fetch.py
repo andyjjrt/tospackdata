166dd1fe2498bd75c14eefb9661418ef32178244
andyjjrt <andyjjrt@gmail.com>
2021-07-23 18:17:03 +0800

    variable fix

--- fetch.py
+++ fetch.py
@@ -13,6 +13,7 @@ print("start from " + str(datetime.utcnow()))
 
 uid = os.environ['TOS_UID']
 auth = os.environ['TOS_AUTH']
+
 with open('data/data.json', 'w') as f:
     f.write('{"uid":"' + str(uid) + '", "auth":"' + str(auth) + '", "update_time" : "' + str(datetime.utcnow().isoformat()) + '"}')
 
@@ -23,10 +24,11 @@ for src in script:
         target_url = str(src.get('src'))
 
 res = requests.get("https://checkup.tosgame.com" + target_url).text
-monster_data = json.dumps(json5.loads(res.split("var n=new Map(")[1].split(".map((function(a){return[a.id,a]})))}")[0].replace("!", "")))
-monster_active = json.dumps(json5.loads(res.split("var n=new Map(")[2].split(".map((function(a){return[a[0],{name:JSON.parse(a[1].name),description:JSON.parse(a[1].description)}]})))}")[0]))
-monster_leader = json.dumps(json5.loads(res.split("var n=new Map(")[3].split(".map((function(a){return[a[0],{name:JSON.parse(a[1].name),description:JSON.parse(a[1].description)}]})))}")[0]))
-monster_team = json.dumps(json5.loads(res.split("var n=new Map(")[4].split(".map((function(a){return[a[0],JSON.parse(a[1])]})))}")[0]))
+print(target_url)
+monster_data = json.dumps(json5.loads(res.split("var t=new Map(")[1].split(".map((function(a){return[a.id,a]})))}")[0].replace("!", "")))
+monster_active = json.dumps(json5.loads(res.split("var t=new Map(")[2].split(".map((function(a){return[a[0],{name:JSON.parse(a[1].name),description:JSON.parse(a[1].description)}]})))}")[0]))
+monster_leader = json.dumps(json5.loads(res.split("var t=new Map(")[3].split(".map((function(a){return[a[0],{name:JSON.parse(a[1].name),description:JSON.parse(a[1].description)}]})))}")[0]))
+monster_team = json.dumps(json5.loads(res.split("var t=new Map(")[4].split(".map((function(a){return[a[0],JSON.parse(a[1])]})))}")[0]))
 
 with open('data/MONSTER_DATA.json', 'w') as f:
     f.write(monster_data)
